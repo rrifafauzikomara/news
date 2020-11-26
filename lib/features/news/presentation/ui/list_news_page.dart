56b9877b84f597a41a28c0def770bbfce6750f51
rrifafauzikomara <rifafauzi6@gmail.com>
2020-11-26 22:34:59 +0700

    move the file

--- lib/features/news/presentation/ui/list_news_page.dart
+++ lib/features/news/presentation/ui/list_news_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:news/features/news/di/injection_container.dart';
+import 'package:news/core/di/injection_container.dart';
 import 'package:news/features/news/presentation/bloc/bloc.dart';
 import 'package:news/shared/widget/card_article.dart';
 import 'package:news/shared/widget/news_header.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:news/features/news/di/injection_container.dart' as di;
+import 'package:news/core/di/injection_container.dart' as di;
 import 'package:news/core/bloc/news_bloc_observer.dart';
 import 'package:news/features/detail/presentation/ui/detail_news_page.dart';
 import 'package:news/features/home/presentation/ui/home_page.dart';
